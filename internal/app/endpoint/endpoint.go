eaa1b131c01e078ca2889e7995df8b2c86e427d8
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-01 14:21:58 +0300

    Add check in get method if id user not found

--- internal/app/endpoint/endpoint.go
+++ internal/app/endpoint/endpoint.go
@@ -31,19 +31,12 @@ func (e *Endpoint) SearchUsers(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, us.List)
 }
 
-type CreateUserRequest struct {
-	DisplayName string `json:"display_name"`
-	Email       string `json:"email"`
-}
-
-func (c *CreateUserRequest) Bind(r *http.Request) error { return nil }
-
 func (e *Endpoint) CreateUser(w http.ResponseWriter, r *http.Request) {
 	us := e.s.GetUserStore()
 
-	request := CreateUserRequest{}
+	req := entity.CreateUserRequest{}
 
-	if err := render.Bind(r, &request); err != nil {
+	if err := render.Bind(r, &req); err != nil {
 		_ = render.Render(w, r, errors.BadRequest(err.Error()))
 		return
 	}
@@ -51,8 +44,8 @@ func (e *Endpoint) CreateUser(w http.ResponseWriter, r *http.Request) {
 	us.Increment++
 	u := entity.User{
 		CreatedAt:   time.Now(),
-		DisplayName: request.DisplayName,
-		Email:       request.DisplayName,
+		DisplayName: req.DisplayName,
+		Email:       req.DisplayName,
 	}
 
 	id := strconv.Itoa(us.Increment)
@@ -70,14 +63,13 @@ func (e *Endpoint) GetUser(w http.ResponseWriter, r *http.Request) {
 	us := e.s.GetUserStore()
 	id := chi.URLParam(r, "id")
 
+	if _, ok := us.List[id]; !ok {
+		_ = render.Render(w, r, errors.NotFound(errors.USER_NOT_FOUND))
+		return
+	}
+
 	render.JSON(w, r, us.List[id])
 }
-
-type UpdateUserRequest struct {
-	DisplayName string `json:"display_name"`
-}
-
-func (c *UpdateUserRequest) Bind(r *http.Request) error { return nil }
 
 func (e *Endpoint) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	us := e.s.GetUserStore()
