12a8185bbdab9a709d1775aef89dc72391dd5e18
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-02 12:52:45 +0300

    Add create dir option

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -6,13 +6,20 @@ import (
 	"os"
 )
 
-const filename string = "./logs/info.log"
+const (
+	filename string = "./logs/info.log"
+	dirName  string = "logs"
+)
 
 //type Logger struct {
 //	l *log.Logger
 //}
 
 func New() (*log.Logger, error) {
+	if err := os.Mkdir(dirName, 0750); err != nil && !os.IsExist(err) {
+		return nil, fmt.Errorf("New logger: %v", err)
+	}
+
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
 	if err != nil {
 		return nil, fmt.Errorf("New logger: %v", err)
